b0d262b6d5069e47eecdf35679801457039e03c3
JosueOrellan4 <tomiokaorellana@gmail.com>
2024-11-26 11:50:20 -0300

    api_01

---
+++ roupabox/API.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roupabox
+{
+    internal class API
+    {
+        static async Task Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+
+            string apiUrl = "https://fakestoreapi.com/products";
+
+            try
+            {
+                HttpsResponseMessage response = await client.GetAsync(apiUrl);
+
+
+            }
+        }
+    }
+}
